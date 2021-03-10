156b2182a545431c944c3492582adb00653a5281
david.muolo <davidmuolo1@gmail.com>
2021-03-10 08:28:45 -0600

    Update

--- FibonacciEvenSum/Program.cs
+++ FibonacciEvenSum/Program.cs
@@ -14,21 +14,24 @@ namespace FibonacciEvenSum
             //1 + 1 = 2;
             //2 + 1 = 3;
             //3 + 2 = 5;
-            //5 + 3 = 8;x  y  z
-            //8 + 5 = 13;z  x  (z+x) = y?
-            //13 + 8 = 21;y + z (y + z = x)?
+            //5 + 3 = 8;
+            //8 + 5 = 13;
+            //13 + 8 = 21;
             //21 + 13 = 34;
             //34 + 21 = 55;
             //55 + 34 = 39;
 
             //Sum of even-numbered fibonacci sequence values under 4,000,000
 
-            //if statement to test if divisible by 2?
             long x = 0;
             long y = 1;
             long z = 2;
 
             List<long> fibonacciNums = new List<long>();
+
+            Console.WriteLine();
+            Console.WriteLine("ALL NUMS");
+            Console.WriteLine();
 
             do
             {
@@ -51,12 +54,18 @@ namespace FibonacciEvenSum
                 }
             }
 
+            Console.WriteLine();
+            Console.WriteLine("EVEN NUMS");
+            Console.WriteLine();
             foreach (long a in evenFibonacciNums)
             {
                 Console.WriteLine(a);
             }
             Console.WriteLine();
+            Console.WriteLine("SUM OF EVEN NUMS");
+            Console.WriteLine();
             Console.WriteLine($"= " + evenFibonacciNums.Sum());
+            Console.WriteLine();
 
             //long fibonacci = 1;
 
