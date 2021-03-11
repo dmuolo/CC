12199336a9c1f51a466cd7825ead7a4fea825b15
david.muolo <davidmuolo1@gmail.com>
2021-03-11 08:47:30 -0600

    Hello

--- TenDigitFibonacci/Program.cs
+++ TenDigitFibonacci/Program.cs
@@ -17,7 +17,7 @@ namespace TenDigitFibonacci
 
             //What is the index of the first term in the Fibonacci sequence to contain 10 digits?
 
-            long xx = 0;
+            long x = 0;
             long y = 1;
             long z = 2;//set up 3 variables for fibonacci sequence
 
@@ -25,13 +25,13 @@ namespace TenDigitFibonacci
 
             do
             {
-                z = xx + y;
+                z = x + y;
                 fibonacciNums.Add(z);
-                y = z + xx;
+                y = z + x;
                 fibonacciNums.Add(y);
-                xx = y + z;
-                fibonacciNums.Add(xx);//storing fibonacci sequence #s in list
-            } while (xx < 1000000000 && y < 1000000000 && z < 1000000000); //trading the val
+                x = y + z;
+                fibonacciNums.Add(x);//storing fibonacci sequence #s in list
+            } while (x < 1000000000); //trading the val
 
             List<string> lengths = new List<string>();//declared list of strings
 
@@ -42,7 +42,7 @@ namespace TenDigitFibonacci
 
             foreach (string a in lengths)
             {
-                if (lengths.All(x => x.Length < 11))//x evaluates to all items in List "lengths"  where the length is less than 11
+                if (lengths.All(b => b.Length < 11))//x evaluates to all items in List "lengths"  where the length is less than 11
                     //Just so happens there is only one item in the fibonacci sequence with a length of 10
                 {
                     Console.WriteLine("[" + lengths.IndexOf(a) + "] " + " " + a + " " + $" Length: {a.Length}");//displays the index, #, and length of # for each # in the fibonacci sequence up to the solution. //Only issue present is that the 2nd index also shows as "0" since both are equal to 1.
